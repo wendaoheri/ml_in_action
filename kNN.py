b966394518934962a8d53845fcfbd8edeb1679dd
xiangliu <xiangliu@xianglius-MacBook-Air.local>
2015-06-08 17:46:18 +0800

    1

--- kNN.py
+++ kNN.py
@@ -32,6 +32,6 @@ def file2matrix(filename):
 		line = line.strip()
 		listFromLine = line.split('\t')
 		returnMat[index] = listFromLine[0: 3]
-		classLabelVector.append(listFromLine[-1])
+		classLabelVector.append(int(listFromLine[-1]))
 		index += 1
 	return returnMat, classLabelVector
--- kNNTest.py
+++ kNNTest.py
@@ -1,5 +1,5 @@
 __author__ = 'xiangliu'
 import kNN
 
-datingDataMat, datingLabels = kNN.file2matrix('datingTestSet.txt')
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
 print datingDataMat
