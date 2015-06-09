b4cc37998a25022f07ec3fad412853916be69833
xiangliu <xiangliu@xianglius-MacBook-Air.local>
2015-06-09 12:00:45 +0800

    add trees

--- kNN.py
+++ kNN.py
@@ -35,3 +35,29 @@ def file2matrix(filename):
 		classLabelVector.append(int(listFromLine[-1]))
 		index += 1
 	return returnMat, classLabelVector
+
+def autoNorm(dataset):
+	minVals = dataset.min(0)
+	maxVals = dataset.max(0)
+	ranges = maxVals - minVals
+	normDataset = np.zeros(dataset.shape)
+	m = dataset.shape[0]
+	normDataset = dataset - np.tile(minVals, (m, 1))
+	normDataset /= np.tile(ranges, (m, 1))
+	return normDataset, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.1
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m * hoRatio)
+	errorCount = 1.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, ], datingLabels[numTestVecs: m], 3)
+		print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+		if classifierResult != datingLabels[i]:
+			errorCount += 1.0
+	print "the total error rate is : %f" % (errorCount / float(numTestVecs))
+
+datingClassTest()
--- kNNTest.py
+++ kNNTest.py
@@ -1,5 +1,13 @@
 __author__ = 'xiangliu'
 import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-print datingDataMat
+normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+print normMat
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * np.array(datingLabels), 15.0 * np.array(datingLabels))
+# plt.show()
