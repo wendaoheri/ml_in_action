6d1fa4224d8bc7228ad2f5f51cfefa5533475a0e
wendaoheri <wendaoheri@gmail.com>
2015-06-09 15:18:40 +0800

    add treeplot

--- kNNTest.py
+++ kNNTest.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
-print normMat
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * np.array(datingLabels), 15.0 * np.array(datingLabels))
-# plt.show()
+# normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+# print normMat
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * np.array(datingLabels), 15.0 * np.array(datingLabels))
+plt.show()
--- trees.py
+++ trees.py
@@ -1,5 +1,6 @@
 __author__ = 'xiangliu'
 from math import log
+import operator
 
 
 def calcShannonEnt(dataSet):
@@ -39,5 +40,52 @@ def splitDataSet(dataSet, axis, value):
 			retDataset.append(reducedFeatVec)
 	return retDataset
 
+
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0]) - 1
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0
+	bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+		for value in uniqueVals:
+			subDataSet = splitDataSet(dataSet, i, value)
+			prob = len(subDataSet) / float(len(dataSet))
+			newEntropy += prob * calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntropy
+		if infoGain > bestInfoGain:
+			bestInfoGain = infoGain
+			bestFeature = i
+	return bestFeature
+
+def majorityCnt(classList):
+	classCount = {}
+	for vote in classList:
+		if vote not in classCount:
+			classCount[vote] = 1
+		else:
+			classCount[vote] += 1
+	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+	return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+	classList = [example[-1] for example in dataSet]
+	if classList.count(classList[0]) == len(classList):
+		return classList[0]
+	if len(dataSet[0]) == 1:
+		return majorityCnt(classList)
+	bestFeat = chooseBestFeatureToSplit(dataSet)
+	bestFeatLabel = labels[bestFeat]
+	myTree = {bestFeatLabel: {}}
+	del(labels[bestFeat])
+	featValues = [example[bestFeat] for example in dataSet]
+	uniqueVals = set(featValues)
+	for value in uniqueVals:
+		subLabels = labels[:]
+		myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+	return myTree
+
 dataMat, labels = createDataSet()
-print splitDataSet(dataMat, 0, 0)
+print createTree(dataMat, labels)
